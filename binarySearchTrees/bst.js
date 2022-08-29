a4ef94de069c3b08bd94d4b368d23d306b0fc05e
gcerv93 <gcerv93@protonmail.com>
2022-08-29 12:52:33 -0500

    Add buildTree method with helper functions

--- binarySearchTrees/bst.js
+++ binarySearchTrees/bst.js
@@ -1,13 +1,62 @@
 const nodeFactory = (data) => {
   return {
     data: data,
-    leftChild: null,
-    rightChild: null,
+    left: null,
+    right: null,
   };
 };
 
 const treeFactory = (array) => {
+  const sortAndRemoveDups = (arr) => {
+    let sorted = arr.sort((a, b) => a - b);
+
+    let result = [];
+    sorted.forEach((num) => {
+      if (!result.includes(num)) {
+        result.push(num);
+      }
+    });
+
+    return result;
+  };
+
+  const buildTree = (arr) => {
+    let arrSorted = sortAndRemoveDups(arr);
+
+    if (arrSorted.length === 0) {
+      return null;
+    }
+
+    let start = 0;
+    let end = arrSorted.length - 1;
+    let mid = Math.floor((start + end) / 2);
+
+    const root = nodeFactory(arrSorted[mid]);
+
+    root.left = buildTree(arrSorted.slice(0, mid));
+    root.right = buildTree(arrSorted.slice(mid + 1));
+
+    return root;
+  };
+
   return {
-    root: buildTree(),
+    root: buildTree(array),
   };
 };
+
+// array data for tree
+// [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+let tree = treeFactory([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+
+prettyPrint(tree.root);
